93e603804480cf6303751400248b9468086e99d6
Adam Piech <adampiech7@op.pl>
2016-03-20 23:16:35 +0100

    test event handler buttons

--- app/src/main/java/com/calculator/adampiech/calculator/Calc.java
+++ app/src/main/java/com/calculator/adampiech/calculator/Calc.java
@@ -4,13 +4,78 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Calc extends Activity {
+
+    TextView calcView;
+    TextView resultView;
+
+    Button buttonOne;
+    Button buttonTwo;
+    Button buttonThree;
+
+    Button buttonClean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_calc);
+
+        calcView = (TextView)findViewById(R.id.calcView);
+        resultView = (TextView)findViewById(R.id.resultView);
+
+        buttonOne = (Button)findViewById(R.id.buttonOne);
+        buttonTwo = (Button)findViewById(R.id.buttonTwo);
+        buttonThree = (Button)findViewById(R.id.buttonThree);
+
+        buttonClean = (Button)findViewById(R.id.buttonClean);
+
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ("0".equals(calcView.getText().toString())) {
+                    calcView.setText("");
+                    resultView.setText("");
+                }
+                calcView.setText(calcView.getText().toString() +  buttonOne.getText().toString());
+                resultView.setText(resultView.getText().toString() + buttonOne.getText().toString());
+            }
+        });
+
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ("0".equals(calcView.getText().toString())) {
+                    calcView.setText("");
+                    resultView.setText("");
+                }
+                calcView.setText(calcView.getText().toString() +  buttonTwo.getText().toString());
+                resultView.setText(resultView.getText().toString() + buttonTwo.getText().toString());
+            }
+        });
+
+        buttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ("0".equals(calcView.getText().toString())) {
+                    calcView.setText("");
+                    resultView.setText("");
+                }
+                calcView.setText(calcView.getText().toString() +  buttonThree.getText().toString());
+                resultView.setText(resultView.getText().toString() + buttonThree.getText().toString());
+            }
+        });
+
+        buttonClean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calcView.setText("0");
+                resultView.setText("0");
+            }
+        });
     }
 
     @Override
